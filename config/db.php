a379f51cd66a27136026d8802b88ea0dddb98165
Gueorgui Agapov <7s4r@users.noreply.github.com>
2016-06-26 09:45:44 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 define('DB_DRIVER', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'ebidix');
-define('DB_USER', 'giorgio');
-define('DB_PASSWD', 'russia1987!');
+define('DB_USER', 'user');
+define('DB_PASSWD', 'password');
 define('DB_PREFIX', 'eb_');
 ?>
