9fbc8007e72cb5e4ef7b3c21238c9c3fa1434fa2
bwebmedia <contact@bwebmedia.com>
2016-10-23 10:03:54 +0000

    modify rights

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 define('DB_DRIVER', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'ebidix');
-define('DB_USER', 'root');
-define('DB_PASSWD', '');
+define('DB_USER', 'gio');
+define('DB_PASSWD', 'V4!4zRGK');
 define('DB_PREFIX', 'eb_');
 ?>
